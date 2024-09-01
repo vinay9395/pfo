26c6d0a266fd8dbbc57d5e7fcd313c7aa6506b5f
vinay9395 <163309318+vinay9395@users.noreply.github.com>
2024-09-01 22:12:56 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/Vanshfr69",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@Vanshfr69",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:vanshfr69@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "vanshfr69@gmail.com",
 	},
 	{
-		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
-		label: "Github",
-		handle: "chronark",
+		icon: <Telegram size={20} />,
+		href: "https://T.me/Vanshfr",
+		label: "Telegram",
+		handle: "Vanshfr",
 	},
 ];
 
