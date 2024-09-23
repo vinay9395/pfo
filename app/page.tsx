df416a8b09e40da2fd741f82d560a100b0c63c8f
vinay9395 <163309318+vinay9395@users.noreply.github.com>
2024-09-23 21:44:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Particles from "./components/particles";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Particles from "./components/particles";
